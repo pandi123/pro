362bec06b12d58ab960da9b204d4e6824cb843af
pandi123 <pandikumar201295@gmail.com>
2016-08-07 23:06:20 -0700

    king.java
    dfsad

---
+++ README.java
@@ -0,0 +1,19 @@
+public class test
+{
+public static void main(String[]args)
+{
+int a[]=new a[30];
+int b[]=new b[30];
+
+for(int i=0;i<a.length;i++)
+{
+for(int j=i+1;j<a.length;j++)
+{
+if(a[i]==a[j])
+{
+return a[i];
+}
+}
+}return 0;
+}
+}
